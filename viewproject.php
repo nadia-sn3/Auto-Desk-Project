dd00b6c489a6d3d55cadd013e95a66a6b78ba742
misbxh <c3009611@hallam.shu.ac.uk>
2025-02-18 11:38:14 +0000

    Account

--- viewproject.php
+++ viewproject.php
@@ -1,38 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/base.css">
-    <title>AutoDesk | Project Name</title>
-</head>
-<body>
-
-<div class="page-container">   
-
-<div class = "project-container">
-    <div class = "project-header">
-        <div class = "project-title"></div>
-        <div class = "project-buttons"></div>
-    </div>
-    <div class = "project-model">
-        <div class = "project-model-viewer">
-
-        </div>
-        <div class = "project-model-data">
-
-        </div>
-    </div>
-    <div class = "project-model-timeline">
-
-    </div>
-</div>
-</div>
-
-</body>
-</html> -->
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
