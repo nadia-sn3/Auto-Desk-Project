b9f5ba9961211be499b7b48fa9f0bbed683fe6ab
misbxh <c3009611@hallam.shu.ac.uk>
2025-02-18 13:57:53 +0000

    Version corrected including thumbnail, commit, description

--- version.php
+++ version.php
@@ -1,39 +1,18 @@
 <link rel="stylesheet" href="style/version.css">
-<div class="project-model-timeline-versions">
-    <div class="timeline-version-">
-        <div class="user-info">
-            <img src="images/UserIcon.png" alt="User Icon" class="user-icon">
-            <span class="username">{Username}</span>
-            <button class="toggle-arrow" onclick="toggleInfo(this)">▶</button> 
-        </div>
-        <div class="extra-info">
-            <img src="path/to/thumbnail.jpg" alt="Thumbnail" class="thumbnail">
-            <p class="description">Description of version 1</p>
-        </div>
-    </div>
-
-    <div class="timeline-version-">
-        <div class="user-info">
-            <img src="images/UserIcon.png" alt="User Icon" class="user-icon">
-            <span class="username">{Username}</span>
-            <button class="toggle-arrow" onclick="toggleInfo(this)">▶</button>
-        </div>
-        <div class="extra-info">
-            <img src="path/to/thumbnail.jpg" alt="Thumbnail" class="thumbnail">
-            <p class="description">Description of version 2</p>
-        </div>
-    </div>
 
     <div class="timeline-version-">
         <div class="user-info">
             <img src="images/UserIcon.png" alt="User Icon" class="user-icon">
             <span class="username">{Username}</span>
             <button class="toggle-arrow" onclick="toggleInfo(this)">▶</button> 
+            <span class="version-heading">Version 12.0.1</span> 
         </div>
+        <h4>Car / Engine / Engine Part X</h4>
+        <h5 class="commit-message">Commit Message</h5>        
+        <p> Bug fixes and performance improvements</p> 
         <div class="extra-info">
             <img src="path/to/thumbnail.jpg" alt="Thumbnail" class="thumbnail">
-            <p class="description">Description of version 3</p>
         </div>
     </div>
-</div>
+
 <script src="js/version-dropdown.js"></script>
