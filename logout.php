d4b9a9fef971eb4bcc88cabbae501361ccfa6f8a
Nadia Sajjad <c2020536@hallam.shu.ac.uk>
2025-03-26 09:58:08 +0000

    Enhanced logout logic

--- logout.php
+++ logout.php
@@ -1,7 +1,25 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+$_SESSION = array();
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(), 
+        '', 
+        time() - 42000,
+        $params["path"], 
+        $params["domain"],
+        $params["secure"], 
+        $params["httponly"]
+    );
+}
+
 session_destroy();
-setcookie(session_name(), '', time() - 3600, '/'); 
+session_regenerate_id(true);
 
 header("Location: signin.php"); 
 exit();
