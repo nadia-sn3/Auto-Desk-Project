55894a8f23681002754517f744cea2fd4e0f919c
misbxh <c3009611@hallam.shu.ac.uk>
2025-03-27 21:31:41 +0000

    delete button design for admin

--- js/manage-users.js
+++ js/manage-users.js
@@ -7,8 +7,8 @@ let userIdToDelete = null;
 
 function confirmDelete(event, userId) {
     event.preventDefault(); 
-    userIdToDelete = userId; 
-    modal.style.display = "block"; 
+    userIdToDelete = userId;
+    modal.style.display = "block";
 }
 
 closeBtn.onclick = function() {
@@ -30,3 +30,11 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 };
+
+document.querySelector('.open-modal-btn').addEventListener('click', function() {
+    document.getElementById('deleteModal').style.display = 'block';
+});
+
+document.querySelector('.close-btn').addEventListener('click', function() {
+    document.getElementById('deleteModal').style.display = 'none';
+});
