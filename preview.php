8fe17d45bea95ea7451c19f981b7854f054c534c
Nadia Sajjad <c2020536@hallam.shu.ac.uk>
2025-03-27 12:56:12 +0000

    Fixed redirection

--- preview.php
+++ preview.php
@@ -8,7 +8,7 @@
         <h3><?= htmlspecialchars($project_name) ?></h3>
         <p><?= htmlspecialchars($description) ?></p>
         <div class="model-preview-actions">
-            <a href="view-project.php?project_id=<?= $project_id ?>" class="btn-view">View</a>
+            <a href="file-list.php?project_id=<?= $project_id ?>" class="btn-view">View</a>
             <a href="#" class="btn-download">Download</a>
         </div>
     </div>
